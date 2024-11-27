55b5a73bf59067fa9592522f797df0a5c0bb350e
James Tuling <151947625+Sssstoic@users.noreply.github.com>
2024-11-27 12:31:21 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
-import LoginPage from "./login/page"; // Import LoginPage component
-
+import LoginPage from "./login/page";
 const HomePage = () => {
   return <LoginPage />; // Render the LoginPage component directly
 };
